51873d325026b6fcadf40da836be4e1a78e6e57a
1yco <jlby1.xiii@gmail.com>
2020-10-29 00:15:34 +0900

    change type

--- src/components/molecules/InputBaseAttack.vue
+++ src/components/molecules/InputBaseAttack.vue
@@ -1,21 +1,28 @@
 <template>
-  <v-text-field v-model="base_attack_val" label="基礎攻撃力(キャラ+武器)" placeholder="1234"></v-text-field>
+  <v-text-field
+    v-model.trim="base_attack_val"
+    label="基礎攻撃力(キャラ+武器)"
+    type="number"
+    min="0"
+    placeholder="1234"
+  ></v-text-field>
 </template>
 
 <script>
 export default {
+  name: "InputBaseAttack",
   props: {
     base_attack: {
-      type: String
+      type: Number
     }
   },
   computed: {
     base_attack_val: {
-      get() {
-        return this.base_attack;
-      },
+      // get() {
+      //   return this.base_attack;
+      // },
       set(value) {
-        this.$emit("change:value", value);
+        this.$emit("change:base_attack", value);
       }
     }
   }
