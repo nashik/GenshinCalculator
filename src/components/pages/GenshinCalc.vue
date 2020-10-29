ddbc6bea89fcab9f0cc206b9c3873b67fac98158
1yco <jlby1.xiii@gmail.com>
2020-10-30 01:22:22 +0900

    delete CheckCard

--- src/components/pages/GenshinCalc.vue
+++ src/components/pages/GenshinCalc.vue
@@ -3,7 +3,7 @@
     <v-app-bar color="primary" dark app>
       <Header />
     </v-app-bar>
-    <v-content>
+    <v-main>
       <Input
         @change:total_attack="total_attack=$event"
         @change:critical_rate="critical_rate=$event"
@@ -14,34 +14,7 @@
         :critical_rate="critical_rate"
         :critical_damage="critical_damage"
       />
-      <v-card width="500px" class="mx-auto my-10">
-        <v-card-title>値の受け渡し確認(Pages)</v-card-title>
-        <v-simple-table dense>
-          <template v-slot:default>
-            <thead>
-              <tr>
-                <th class="text-left">Name</th>
-                <th class="text-left">Value</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>基礎攻撃力</td>
-                <td>{{ total_attack }}</td>
-              </tr>
-              <tr>
-                <td>会心率(%)</td>
-                <td>{{ critical_rate }}</td>
-              </tr>
-              <tr>
-                <td>会心ダメージ(%)</td>
-                <td>{{ critical_damage }}</td>
-              </tr>
-            </tbody>
-          </template>
-        </v-simple-table>
-      </v-card>
-    </v-content>
+    </v-main>
     <v-footer color="primary" dark app>
       <Footer />
     </v-footer>
